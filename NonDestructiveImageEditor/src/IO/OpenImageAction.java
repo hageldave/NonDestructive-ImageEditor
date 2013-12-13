cc89883507dab405139c640c572b14e2b60a8296
hageldave <msv87749@stud.uni-stuttgart.de>
2013-12-13 02:42:36 +0100

    out-commented opening with JFileChooser

--- NonDestructiveImageEditor/src/IO/OpenImageAction.java
+++ NonDestructiveImageEditor/src/IO/OpenImageAction.java
@@ -9,7 +9,10 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.AbstractAction;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
 
 import manipulators.image.ImageLayer;
 import model.DataOverview;
@@ -27,6 +30,28 @@ public class OpenImageAction extends AbstractAction {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
+//		JFileChooser filechooser = new JFileChooser(FileSystemView.getFileSystemView());
+//		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+//		        "Image Files", "jpg", "gif", "png", "bmp");
+//		filechooser.setFileFilter(filter);
+//		int returnVal = filechooser.showOpenDialog(frame);
+//	    if(returnVal == JFileChooser.APPROVE_OPTION) {
+//	    	File f = filechooser.getSelectedFile();
+//	    	if(f!= null && f.exists()){
+//				try {
+//					FileInputStream in = new FileInputStream(f.getAbsolutePath());
+//					BufferedImage image = ImageIO.read(in);
+//					if(image != null){
+//						DataOverview.renderer.addManipulation(0, new ImageLayer(new PixelArray(image)));
+//					}
+//				} catch (IOException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
+//			}
+//			frame.requestFocus();
+//	    }
+		
 		FileDialog filedialog = new FileDialog(frame, "Load Image", FileDialog.LOAD);
 		filedialog.setMultipleMode(false);
 		filedialog.setVisible(true);
