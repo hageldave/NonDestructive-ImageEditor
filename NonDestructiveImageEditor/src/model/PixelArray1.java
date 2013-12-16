ee79808188eee8f3921aec0c46b96e141bed0373
hageldave <msv87749@stud.uni-stuttgart.de>
2013-12-16 03:31:17 +0100

    replaced old PixelArray (now PixelArray1) with new one which booooooosts performance to a whole new level

--- NonDestructiveImageEditor/src/model/PixelArray1.java
+++ NonDestructiveImageEditor/src/model/PixelArray1.java
@@ -1,21 +1,20 @@
 package model;
 
 import java.awt.image.BufferedImage;
-
-public class PixelArray implements IPixelCollection {
-	private long[] rgbaPixels = new long[0];
+/** Deprecated since it is waaaay slower than current implementation of {@link PixelArray}
+ */
+@Deprecated
+public class PixelArray1 implements IPixelCollection {
+	private int[][] rgbaPixels = new int[0][4];
 	private int imageWidth = 0;
 	private int divider = 1;
-	
-	protected static final int minNegative16 = -21845; // = -(2^16)/3
-	protected static final int maxPositive16 = 43689; // = 2*(2^16)/3 -1 or 2^16 + minNegative16 -1
 	
 	/* (non-Javadoc)
 	 * @see model.IPixelCollection#getRGBA(int, int)
 	 */
 	@Override
 	public int[] getRGBA(int x, int y){
-		return separateRGBA16(rgbaPixels[y*imageWidth + x]);
+		return rgbaPixels[y*imageWidth + x];
 	}
 	
 	
@@ -24,7 +23,7 @@ public class PixelArray implements IPixelCollection {
 	 */
 	@Override
 	public int[] getRGBAatIndex(int index){
-		return separateRGBA16(rgbaPixels[index]);
+		return rgbaPixels[index];
 	}
 	
 	/* (non-Javadoc)
@@ -32,7 +31,7 @@ public class PixelArray implements IPixelCollection {
 	 */
 	@Override
 	public void setRGBA(int x, int y, int[] rgba){
-		rgbaPixels[y*imageWidth + x] = recombineRGBA16(rgba);
+		rgbaPixels[y*imageWidth + x] = rgba;
 	}
 	
 	/* (non-Javadoc)
@@ -40,12 +39,7 @@ public class PixelArray implements IPixelCollection {
 	 */
 	@Override
 	public void setRGBAatIndex(int index, int[] rgba){
-		rgbaPixels[index] = recombineRGBA16(rgba);
-	}
-	
-	
-	protected void setRGBA16(int x, int y, long rgba16){
-		rgbaPixels[y*imageWidth + x] = rgba16;
+		rgbaPixels[index] = rgba;
 	}
 	
 	/* (non-Javadoc)
@@ -77,12 +71,12 @@ public class PixelArray implements IPixelCollection {
 	}
 	
 	
-	public PixelArray(int width, int height) {
+	public PixelArray1(int width, int height) {
 		this.imageWidth = width;
-		this.rgbaPixels = new long[width*height];
+		this.rgbaPixels = new int[width*height][4];
 	}
 
-	public PixelArray(long[] rgbaPixels, int imageWidth) {
+	public PixelArray1(int[][] rgbaPixels, int imageWidth) {
 		this.rgbaPixels = rgbaPixels;
 		if(imageWidth < 0){
 			throw new RuntimeException("PixelArray cannot have negative width: " + imageWidth);
@@ -91,44 +85,47 @@ public class PixelArray implements IPixelCollection {
 		}
 	}
 	
-	public PixelArray(PixelArray pixArr) {
+	public PixelArray1(PixelArray1 pixArr) {
 		this.imageWidth = pixArr.imageWidth;
-		this.rgbaPixels = new long[pixArr.getNumOfPixels()];
+		this.rgbaPixels = new int[pixArr.getNumOfPixels()][4];
 		for(int i = 0; i < pixArr.rgbaPixels.length; i++){
-			// copy values (dont copy array!)
-			rgbaPixels[i] = pixArr.rgbaPixels[i];
+			// copy values (dont copy rgba int[]!)
+			rgbaPixels[i][0] = pixArr.rgbaPixels[i][0];
+			rgbaPixels[i][1] = pixArr.rgbaPixels[i][1];
+			rgbaPixels[i][2] = pixArr.rgbaPixels[i][2];
+			rgbaPixels[i][3] = pixArr.rgbaPixels[i][3];
 		}
 	}
 	
-	public PixelArray(IPixelCollection pixArr, int divider) {
+	public PixelArray1(IPixelCollection pixArr, int divider) {
 		this.divider = divider;
 		imageWidth = pixArr.getWidth()/divider;
-		rgbaPixels = new long[(pixArr.getWidth()/divider)*(pixArr.getHeight()/divider)];
+		rgbaPixels = new int[(pixArr.getWidth()/divider)*(pixArr.getHeight()/divider)][4];
 		for(int y = 0; y < pixArr.getHeight()/divider; y++){
 			for(int x = 0; x < pixArr.getWidth()/divider; x++){
 				int[] rgba = pixArr.getRGBA(x*divider, y*divider);
-				setRGBA16(x, y, recombineRGBA16(rgba));
+				setRGBA(x, y, new int[]{rgba[0],rgba[1],rgba[2],rgba[3]});
 			}
 		}
 	}
 	
-	public PixelArray(BufferedImage img){
+	public PixelArray1(BufferedImage img){
 		imageWidth = img.getWidth();
-		rgbaPixels = new long[img.getWidth()*img.getHeight()];
+		rgbaPixels = new int[img.getWidth()*img.getHeight()][4];
 		for(int y = 0; y < img.getHeight(); y++){
 			for(int x = 0; x < img.getWidth(); x++){
-				setRGBA16(x, y, recombineRGBA16(separateRGBA(img.getRGB(x, y))));
+				setRGBA(x, y, separateRGBA(img.getRGB(x, y)));
 			}
 		}
 	}
 	
-	public PixelArray(BufferedImage img, int divider){
+	public PixelArray1(BufferedImage img, int divider){
 		this.divider = divider;
 		imageWidth = img.getWidth()/divider;
-		rgbaPixels = new long[(img.getWidth()/divider)*(img.getHeight()/divider)];
+		rgbaPixels = new int[(img.getWidth()/divider)*(img.getHeight()/divider)][4];
 		for(int y = 0; y < img.getHeight()/divider; y++){
 			for(int x = 0; x < img.getWidth()/divider; x++){
-				setRGBA16(x, y, recombineRGBA16(separateRGBA(img.getRGB(x*divider, y*divider))));
+				setRGBA(x, y, separateRGBA(img.getRGB(x*divider, y*divider)));
 			}
 		}
 	}
@@ -149,10 +146,10 @@ public class PixelArray implements IPixelCollection {
 	
 	protected static int[] separateRGBA(int rgba){
 		int[] rgbaArray = new int[4];
-		rgbaArray[3] = (rgba >> 24) & 0xFF;
 		rgbaArray[0] = (rgba >> 16) & 0xFF;
 		rgbaArray[1] = (rgba >> 8) & 0xFF;
 		rgbaArray[2] = rgba & 0xFF;
+		rgbaArray[3] = (rgba >> 24) & 0xFF;
 		return rgbaArray;
 	}
 	
@@ -164,38 +161,12 @@ public class PixelArray implements IPixelCollection {
 		return rgbCompound;
 	}
 	
-	public static int[] fixOutOfBounds(int[] rgba){
+	protected static int[] fixOutOfBounds(int[] rgba){
 		rgba[0] = rgba[0]>0 ? (rgba[0]<255 ? rgba[0]:255):0;
 		rgba[1] = rgba[1]>0 ? (rgba[1]<255 ? rgba[1]:255):0;
 		rgba[2] = rgba[2]>0 ? (rgba[2]<255 ? rgba[2]:255):0;
 		rgba[3] = rgba[3]>0 ? (rgba[3]<255 ? rgba[3]:255):0;
 		return rgba;
 	}
-	
-	protected static int[] separateRGBA16(long rgba16){
-		int[] rgbaArray = new int[4];
-		rgbaArray[3] = (int)((rgba16 >> 48) & 0xFFFF)+minNegative16;
-		rgbaArray[0] = (int)((rgba16 >> 32) & 0xFFFF)+minNegative16;
-		rgbaArray[1] = (int)((rgba16 >> 16) & 0xFFFF)+minNegative16;
-		rgbaArray[2] = (int)((rgba16 & 0xFFFF))+minNegative16;
-		return rgbaArray;
-	}
-	
-	protected static long recombineRGBA16(int[] rgba){
-		long rgbCompound = rgba[3]-minNegative16; // alpha channel
-		rgbCompound = (rgbCompound << 16) +rgba[0]-minNegative16; // minNegative16 is used for allowance of negative rgba values
-		rgbCompound = (rgbCompound << 16) +rgba[1]-minNegative16;
-		rgbCompound = (rgbCompound << 16) +rgba[2]-minNegative16;
-		return rgbCompound;
-	}
-	
-	public static int[] fixOutOfBounds16(int[] rgba16){
-		rgba16[0] = rgba16[0]>minNegative16 ? (rgba16[0]<maxPositive16 ? rgba16[0]:maxPositive16) :minNegative16;
-		rgba16[1] = rgba16[1]>minNegative16 ? (rgba16[1]<maxPositive16 ? rgba16[1]:maxPositive16) :minNegative16;
-		rgba16[2] = rgba16[2]>minNegative16 ? (rgba16[2]<maxPositive16 ? rgba16[2]:maxPositive16) :minNegative16;
-		rgba16[3] = rgba16[3]>minNegative16 ? (rgba16[3]<maxPositive16 ? rgba16[3]:maxPositive16) :minNegative16;
-		return rgba16;
-	}
-	
 	
 }
